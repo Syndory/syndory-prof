bbe67ffe28ca10541bf720613aab70dcd8b2f643
Richard <richardanagonou0@gmail.com>
2026-05-06 11:33:15 +0100

    initialisation de la locale française

--- lib/app/app_bootstrap.dart
+++ lib/app/app_bootstrap.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart'; 
+import 'package:intl/date_symbol_data_local.dart';
 
 import 'theme/app_theme.dart';
 import '../features/debug_health/debug_health_screen.dart';
@@ -11,6 +13,13 @@ class AppBootstrap extends StatelessWidget {
     return MaterialApp(
       title: 'Syndory Prof',
       theme: AppTheme.light,
+      locale: const Locale('fr', 'FR'),                    
+      supportedLocales: const [Locale('fr', 'FR')],        
+      localizationsDelegates: const [                      
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
       builder: (context, child) => child ?? const SizedBox.shrink(),
       home: const DebugHealthScreen(),
     );
